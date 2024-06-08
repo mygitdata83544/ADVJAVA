74c792eb616c29d401369013502f1391421a5609
Harshal Mutyalwar <harshalmutyalwar@gmail.com>
2024-06-08 22:13:10 +0530

    Day02 lab Work

---
+++ Day02/electionservlets/src/main/java/com/sunbeam/servlets/LoginServlet.java
@@ -0,0 +1,76 @@
+package com.sunbeam.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sunbeam.daos.UserDao;
+import com.sunbeam.daos.UserDaoImpl;
+import com.sunbeam.pojos.User;
+
+public class LoginServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		processRequest(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		processRequest(req, resp);
+	}
+
+	protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email = req.getParameter("email");
+		String passwd = req.getParameter("passwd");
+		
+		try(UserDao userDao = new UserDaoImpl()) {
+			
+		  User user = userDao.findByEmail(email);
+		  if(user != null && user.getPassword().equals(passwd)) {
+			  
+			  Cookie c = new Cookie("uname", user.getFirstName());
+			  c.setMaxAge(3600);
+			  resp.addCookie(c);
+			  
+			  HttpSession session = req.getSession();
+			  session.setAttribute("curuser", user);
+			  
+			  if(user.getRole().equals("voter")) {
+				  resp.sendRedirect("candlist");
+			  }
+			  
+			  else {
+				  resp.sendRedirect("result");
+			  }
+		  }
+		  else {
+			  resp.setContentType("text/html");
+			  PrintWriter out = resp.getWriter();
+			  out.println("<html>");
+			  out.println("<head>");
+			  out.println("<title>Login Failed</title>");
+			  out.println("</head>");
+			  out.println("<body>");
+			  out.println("Invalid email or password. <br/><br/>");
+			  out.println("<a href='index.html'>Login Again</a>");
+			  out.println("</body>");
+			  out.println("</html>");
+		  }
+	}
+		catch (Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		
+
+	}
+
+}
---
+++ Day02/electionservlets/src/main/java/com/sunbeam/servlets/VoteServlet.java
@@ -0,0 +1,85 @@
+package com.sunbeam.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.tomcat.util.http.parser.Cookie;
+
+import com.sunbeam.daos.CandidateDao;
+import com.sunbeam.daos.CandidateDaoImpl;
+import com.sunbeam.daos.UserDao;
+import com.sunbeam.daos.UserDaoImpl;
+import com.sunbeam.pojos.User;
+
+public class VoteServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		processRequest(req, resp);
+	}
+
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		processRequest(req, resp);
+	}
+	protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		User user = (User) session.getAttribute("curuser");
+		int userId = user.getId();
+		String message = "";
+		
+		if(user.getStatus()==0) {
+			String candidateId = req.getParameter("candidate");
+			int id = Integer.parseInt(candidateId);
+			try(CandidateDao candDao = new CandidateDaoImpl()) {
+				candDao.incrementVote(id);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new ServletException(e);
+			}
+			message = "Your vote is registerd successfully. <br/><br/>";
+			
+			try(UserDao userDao = new UserDaoImpl()) {
+				userDao.updateStatus(userId, true);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new ServletException(e);
+			}
+		}
+		else {
+			message = "You have already voted. <br/><br/>";
+		}
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Voted</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h3>Online Voting</h3>");
+		
+		String userName = "";
+	/*	Cookie[] arr = req.getCookies();
+		if(arr != null) {
+			for (Cookie c : arr) {
+				if(c.getName().equals("uname")) {
+					userName = c.getValue();
+					break;
+				}
+			}
+		}
+		*/
+		out.printf("Hello, %s<hr/>\n", userName);
+		out.println(message);
+		out.println("<a href='logout'>Sign Out</a>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
